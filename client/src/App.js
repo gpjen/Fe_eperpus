62ac7c201c60795aec1e8421501c200ef0a4f2ba
agan95 <nyonggpj9995@gmail.com>
2022-04-26 23:53:44 +0700

    init project

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C5603AQEPHkEXHtQ37g/profile-displayphoto-shrink_400_400/0/1645019984858?e=1656547200&v=beta&t=JPJv59n4xG3gVkqgnrpdf5wyqpC8t9nLgSxkY5WcYcw"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
